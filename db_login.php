4388eb37527f2554c98a99930d58cf6cd3b3356e
Lerka <didkivska@gmail.com>
2015-06-04 02:45:58 -0400

    fixed db config

--- db_login.php
+++ db_login.php
@@ -2,8 +2,7 @@
    $db_host='127.0.0.1';
    $db_database='ARM';
    $db_username='root';
-   $db_password='root';
-   $db_database = 'ARM';
+   $db_password='';
    $connection = mysql_connect($db_host, $db_username, $db_password);
    $db_select = mysql_select_db($db_database);
    mysql_query("SET NAMES utf8");
